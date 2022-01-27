cc0d86f322d636d281e4d042c0c688abf2877e74
Jesmin Akter <jesmin143215@gmail.com>
2022-01-28 01:52:56 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircleAvatar(
+          radius: 210,
+          backgroundColor: Colors.amber,
+          child: CircleAvatar(
+            radius: 200,
+            child: Text('Hello Google',style: TextStyle(fontWeight: FontWeight.bold),),
+            foregroundColor: Colors.yellow,
+            backgroundImage: AssetImage('images/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'),
+            //backgroundImage: NetworImage(''),
+            //minRadius: 100,
+            //maxRadius: 800,
+          ),
+        ),
+      ),
+    );
+  }
+}
